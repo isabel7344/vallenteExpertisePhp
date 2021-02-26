689f442a310059e94da1bd3c70d21b1998028166
unknown <isa.fof76@gmail.com>
2021-02-26 09:58:53 +0100

    mise en fonction de la modal des détailles formations

--- views/calendrier.php
+++ views/calendrier.php
@@ -1,49 +1,93 @@
 <?php
-    require_once("../controllers/calendrierDispoController.php");
-    require("../modeles/training_sessions.php");
-    /*
+require_once("../controllers/calendrierDispoController.php");
+require("../modeles/training_sessions.php");
+/*
     ** fonction qui change les sessions dont il n'y a pas plus de place en rouge et vert libre 
     */
-    function getCssIfEventInDay($eventInDay) {
-        if (empty($eventInDay)) {
-            return "";
-        } else if ($eventInDay['NUMBER_PLACES_TAKEN'] >= $eventInDay['NUMBER_OF_PLACES_TRAINING']) {
-            return 'bg-danger';
-        } else {
-            return 'bg-success';
-        }
+function getCssIfEventInDay($eventInDay)
+{
+    if (empty($eventInDay)) {
+        return "";
+    } else if ($eventInDay['NUMBER_PLACES_TAKEN'] >= $eventInDay['NUMBER_OF_PLACES_TRAINING']) {
+        return 'bg-danger';
+    } else {
+        return 'bg-success';
     }
-    /*
+}
+
+/*
     ** fonction qui change les sessions dont il n'y a pas plus de place en rouge et vert libre pour le boutton 
     */
-    function getCssIfEventInDayButton($eventInDay) {
-        if (empty($eventInDay)) {
-            return "";
-        } else if ($eventInDay['NUMBER_PLACES_TAKEN'] >= $eventInDay['NUMBER_OF_PLACES_TRAINING']) {
-            return 'btn btn-danger';
-        } else {
-            return 'btn btn-success';
-        }
+function getCssIfEventInDayButton($eventInDay)
+{
+    if (empty($eventInDay)) {
+        return "";
+    } else if ($eventInDay['NUMBER_PLACES_TAKEN'] >= $eventInDay['NUMBER_OF_PLACES_TRAINING']) {
+        return 'btn btn-danger';
+    } else {
+        return 'btn btn-success';
     }
-    /*
+}
+
+/*
     ** fonction qui met les cases en gris  qui ne contiennent pas de  jours dans le mois 
     */
-    function getCssForDayNotInCalendar($case, $day) {
-        return $case >=  $_SESSION["calendrier"]->getFirstDayInMonth() && $day <=  $_SESSION["calendrier"]->getNbDayInMonth() ? '' : 'bg-secondary';
-    }
-    /*
+function getCssForDayNotInCalendar($case, $day)
+{
+    return $case >=  $_SESSION["calendrier"]->getFirstDayInMonth() && $day <=  $_SESSION["calendrier"]->getNbDayInMonth() ? '' : 'bg-secondary';
+}
+/*
     ** fonction qui met les cases en gris quand appui sur le bouton
     */
-    function getCssForDayNotInCalendarbutton($case, $day) {
-        return $case >=  $_SESSION["calendrier"]->getFirstDayInMonth() && $day <=  $_SESSION["calendrier"]->getNbDayInMonth() ? '' : 'btn btn-secondary';
-    }
-    /*
+function getCssForDayNotInCalendarbutton($case, $day)
+{
+    return $case >=  $_SESSION["calendrier"]->getFirstDayInMonth() && $day <=  $_SESSION["calendrier"]->getNbDayInMonth() ? '' : 'btn btn-secondary';
+}
+/*
     ** fonction qui affiche le calendrier ....
     */
-    function printDayIfInCalendar ($case, &$day){
-        return $case >=  $_SESSION["calendrier"]->getFirstDayInMonth() && $day <=  $_SESSION["calendrier"]->getNbDayInMonth() ? $day++ : '';
-    }
+function printDayIfInCalendar($case, &$day)
+{
+    return $case >=  $_SESSION["calendrier"]->getFirstDayInMonth() && $day <=  $_SESSION["calendrier"]->getNbDayInMonth() ? $day++ : '';
+}
 ?>
+
+<?php function showButton($case, &$day, $eventInDay)
+{
+    $printingDay = printDayIfInCalendar($case, $day);
+    if ($eventInDay) {
+        $modalID = "modal" . $case;
+?>
+
+        <button type="button" data-toggle="modal" data-target="<?php echo "#" . $modalID ?>" class="<?= getCssForDayNotInCalendarbutton($case, $day) . " " . getCssIfEventInDayButton($eventInDay) ?>">
+            <?php echo $printingDay ?>
+        </button>
+        <div class="modal fade" id="<?php echo $modalID ?>" tabindex="-1" role="dialog" aria-labelledby="<?php echo $modalID . "Label" ?>" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Session de Formation</h4>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                    </div>
+                    <div class="modal-body">
+                    <?php
+                    if(!empty($getEventInDay)) {
+                    foreach ($getEventInDay as $key => $value) {
+                    ?>
+                    <p>Nom de la Formation <?= strtoupper($value["Name_training"]) ?></P>
+                    <p>Date de début de  formation <?= $value["START_DATE_TRAINING"] ?></p>
+                    <p> Date de fin de   formation <?= $value["End_Date_training"] ?></p>
+                    <p> Nombre de places total <?= $value["NUMBER_OF_PLACES_TRAINING"] ?></p>
+                    <p> Nombre de places prises <?= $value["NUMBER_PLACES_TAKEN"] ?></p>
+                    }<?php
+                    }else {
+                        echo "$printingDay";
+                    }
+                    } ?>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+
 
 <!DOCTYPE html>
 <html lang="fr">
@@ -52,27 +96,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-    <script src="https://kit.fontawesome.com/e2a465b2f1.js" crossorigin="anonymous"></script>
     <link rel="icon" href="../assets/img/logoFondBlanc.png">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <title>calendrier</title>
 </head>
 
 <body style="overflow: hidden;">
     <div class=" row justify-content-center" id="reservation">
-        <div class="col-xl-8 col-sm-11">   
+        <div class="col-xl-8 col-sm-11">
             <div class="d-flex justify-content-center mb-1 mt-2">
                 <form action="../controllers/calendrierDispoController.php" method="POST">
-                    <input name="month" type="hidden" value=<?php  $_SESSION["calendrier"]->getMonthName(); ?>>
-                    <input name="year" type="hidden" value=<?php  $_SESSION["calendrier"]->getYear(); ?>>
+                    <input name="month" type="hidden" value=<?php $_SESSION["calendrier"]->getMonthName(); ?>>
+                    <input name="year" type="hidden" value=<?php $_SESSION["calendrier"]->getYear(); ?>>
                     <input name="direction" type="hidden" value="prev">
                     <button style="background-color: none; border: 0px; box-shadow: none" type="submit"> ◀ </button>
                 </form>
-                <span><?=  $_SESSION["calendrier"]->getMonthName()  . " " .  $_SESSION["calendrier"]->getYear() ?></span>
+                <span><?= $_SESSION["calendrier"]->getMonthName()  . " " .  $_SESSION["calendrier"]->getYear() ?></span>
                 <form action="../controllers/calendrierDispoController.php" method="POST">
-                    <input name="month" type="hidden" value=<?php  $_SESSION["calendrier"]->getMonthName(); ?>>
-                    <input name="year" type="hidden" value=<?php  $_SESSION["calendrier"]->getYear(); ?>>
+                    <input name="month" type="hidden" value=<?php $_SESSION["calendrier"]->getMonthName(); ?>>
+                    <input name="year" type="hidden" value=<?php $_SESSION["calendrier"]->getYear(); ?>>
                     <input name="direction" type="hidden" value="next">
                     <button style="background-color: none; border: 0px; box-shadow: none" type="submitt"> ▶ </button>
                 </form>
@@ -97,29 +139,13 @@
                     while ($day <=  $_SESSION["calendrier"]->getNbDayInMonth()) { ?>
                         <tr class="text-center">
                             <?php for ($i = 1; $i <= 7; $i++) { ?>
-                                <?php $eventInDay =  $trainingSession->getEventInDay($day,  $_SESSION["calendrier"]->getMonthIndex(), $_SESSION["calendrier"]->getYear()) ?> 
-                                <td  class="<?= getCssForDayNotInCalendar($case, $day) . " " . getCssIfEventInDay($eventInDay)?>">
-                                    <?= printDayIfInCalendar($case,$day) ?>
-                                    <button type="button" data-toggle="myModal"
-                                                    data-target="myModal" class="<?= getCssForDayNotInCalendarbutton($case, $day) . " " . getCssIfEventInDayButton($eventInDay)?>">                                                                                                            
-                                                </button>
+                                <?php $eventInDay =  $trainingSession->getEventInDay($day,  $_SESSION["calendrier"]->getMonthIndex(), $_SESSION["calendrier"]->getYear()) ?>
+                                <td class="<?= getCssForDayNotInCalendar($case, $day) . " " . getCssIfEventInDay($eventInDay) ?>">
+                                    <?php showButton($case, $day, $eventInDay); ?>
                                 </td>
                             <?php
                                 $case++;
                             } ?>
-                            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-                                <div class="modal-dialog">
-                                <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                                    <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-                                </div>
-                                <div class="modal-body">
-                                    detaille
-                                </div>
-                                </div><!-- /.modal-content -->
-                                </div><!-- /.modal-dialog -->
-                            </div><!-- /.modal -->
                         </tr>
                     <?php } ?>
                 </tbody>
@@ -127,5 +153,11 @@
         </div>
     </div>
 </body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+</script>
+<script src="https://kit.fontawesome.com/e2a465b2f1.js" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 
 </html>
