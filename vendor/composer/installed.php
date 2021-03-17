6ee0de3f76eb092b68bc803cd8ce441d13d71ab1
unknown <isa.fof76@gmail.com>
2021-03-17 18:47:53 +0100

    mise a jour composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'b52593a26480673be308635d2189524df18e96a0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b52593a26480673be308635d2189524df18e96a0',
+    ),
+  ),
+);
